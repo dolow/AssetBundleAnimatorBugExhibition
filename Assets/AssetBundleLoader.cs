3c074e24654bc9ef832cabe67fc755634ad884e3
kuwabara_yuki <kuwabara_yuki@SF-B004026.local>
2017-08-15 20:15:19 +0900

    first commit

---
+++ Assets/AssetBundleLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetBundleLoader : MonoBehaviour
+{
+    void Start ()
+    {
+        StartCoroutine(LoadAssetBundle());
+    }
+
+    IEnumerator LoadAssetBundle()
+    {
+        WWW rootWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_root"));
+
+        yield return rootWww;
+
+        if (rootWww.error != null) {
+            Debug.Log(rootWww.error);
+            yield break;
+        }
+
+        WWW depsWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_dependency"));
+
+        yield return depsWww;
+
+        if (depsWww.error != null) {
+            Debug.Log(rootWww.error);
+            yield break;
+        }
+
+        GameObject go = rootWww.assetBundle.LoadAsset<GameObject>("Root");
+        Instantiate(go);
+    }
+
+    string GetStreamingAssetsUrl(string path)
+    {
+        return "file://" + Application.streamingAssetsPath + path;
+    }
+}
