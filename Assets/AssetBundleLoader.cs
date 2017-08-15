929fea6c8c84398f29ead38b873f04f73216da9f
smith <kuwabara_yuki@SF-B004026.local>
2017-08-15 20:35:06 +0900

    added safe case

--- Assets/AssetBundleLoader.cs
+++ Assets/AssetBundleLoader.cs
@@ -1,17 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Reflection;
 
 public class AssetBundleLoader : MonoBehaviour
 {
     void Start ()
     {
-        StartCoroutine(LoadAssetBundle());
+        
+        //StartCoroutine(LoadSafeAssetBundle());
+        StartCoroutine(LoadUnsafeCase1AssetBundle());
     }
 
-    IEnumerator LoadAssetBundle()
+    IEnumerator LoadSafeAssetBundle()
     {
-        WWW rootWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_root"));
+        WWW www = new WWW(this.GetStreamingAssetsUrl("/ab_test_safecase_root"));
+
+        yield return www;
+
+        if (www.error != null) {
+            Debug.Log(www.error);
+            yield break;
+        }
+
+        WWW depsWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_safecase_deps"));
+
+        yield return depsWww;
+
+        if (depsWww.error != null) {
+            Debug.Log(depsWww.error);
+            yield break;
+        }
+
+        // not crash even if it is not loaded
+        depsWww.assetBundle.LoadAsset<Material>("Material");
+
+        GameObject go = www.assetBundle.LoadAsset<GameObject>("Root");
+        Instantiate(go);
+    }
+    IEnumerator LoadUnsafeCase1AssetBundle()
+    {
+        WWW rootWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_unsafecase1_root"));
 
         yield return rootWww;
 
@@ -20,7 +49,7 @@ public class AssetBundleLoader : MonoBehaviour
             yield break;
         }
 
-        WWW depsWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_dependency"));
+        WWW depsWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_unsafecase1_deps"));
 
         yield return depsWww;
 
@@ -28,6 +57,9 @@ public class AssetBundleLoader : MonoBehaviour
             Debug.Log(rootWww.error);
             yield break;
         }
+
+        // crash if not loaded
+        // depsWww.assetBundle.LoadAsset<AnimationClip>("AnimationClip");
 
         GameObject go = rootWww.assetBundle.LoadAsset<GameObject>("Root");
         Instantiate(go);
