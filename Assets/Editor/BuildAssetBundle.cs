be434944ba38e7dc1691d81ff1b2e9e3778177f0
smith <kuwabara_yuki@SF-B004026.local>
2017-08-15 20:15:19 +0900

    first commit

---
+++ Assets/Editor/BuildAssetBundle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class BuildAssetBundle
+{
+    [MenuItem ("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string dest = UnityEngine.Application.dataPath + "/StreamingAssets";
+
+        if (Directory.Exists(dest))
+            Directory.Delete(dest, recursive:true);
+        
+        Directory.CreateDirectory(dest);
+
+        BuildPipeline.BuildAssetBundles(dest, BuildAssetBundleOptions.None, BuildTarget.StandaloneOSXIntel64);
+    }
+}
