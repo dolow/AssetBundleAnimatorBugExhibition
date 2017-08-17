7944508ba84b780e0354ef11b172386ebe6e4af1
smith <kuwabara_yuki@SF-B004026.local>
2017-08-17 18:35:45 +0900

    added unsafe cases

---
+++ Assets/AssetBundle/UnsafeCase3/AnimationReferer.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class AnimationReferer : MonoBehaviour
+{
+    void Start ()
+    {
+        Transform[] trans = this.gameObject.GetComponentsInChildren<Transform>();
+
+        GameObject cube = null;
+
+        for (int i = 0; i < trans.Length; i++) {
+            if (trans[i].gameObject.name == "Cube") {
+                cube = trans[i].gameObject;
+                break;
+            }
+        }
+
+        if (cube == null) {
+            Debug.Log("cube == null");
+            return;
+        }
+
+        MeshRenderer meshRenderer = cube.gameObject.GetComponent<MeshRenderer>();
+
+        if (meshRenderer == null) {
+            Debug.Log("meshRenderer == null");
+            return;
+        }
+
+        Material[] materials = meshRenderer.materials;
+
+        Debug.Log("materials.Length : " + materials.Length);
+
+        for (int i = 0; i < materials.Length; i++) {
+            Debug.Log("materials[i].name : " + materials[i].name);
+        }
+
+        Animator anim = this.gameObject.GetComponent<Animator>();
+
+        if (anim == null) {
+            Debug.Log("anim == null");
+            return;
+        }
+
+        if (anim.runtimeAnimatorController == null) {
+            Debug.Log("anim.runtimeAnimatorController == null");
+            return;
+        }
+
+        AnimationClip[] clips = anim.runtimeAnimatorController.animationClips;
+
+        Debug.Log("clips.Length : " + clips.Length);
+
+        for (int i = 0; i < clips.Length; i++) {
+            Debug.Log("clips[i].name : " + clips[i].name);
+        }
+    }
+}
--- Assets/AssetBundleLoader.cs
+++ Assets/AssetBundleLoader.cs
@@ -1,19 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class AssetBundleLoader : MonoBehaviour
 {
-    void Start ()
+    private void Start()
     {
-        
-        //StartCoroutine(LoadSafeAssetBundle());
-        StartCoroutine(LoadUnsafeCase1AssetBundle());
+        Canvas   canvas  = GameObject.FindObjectOfType<Canvas>();
+        Button[] buttons = canvas.gameObject.GetComponentsInChildren<Button>();
+
+        System.Type thisType = this.GetType();
+
+        MethodInfo[] methods = thisType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+         
+        for (int i = 0; i < methods.Length; i++) {
+            MethodInfo method = methods[i];
+
+            if (method.ReturnType.FullName != typeof(IEnumerator).FullName)
+                continue;
+            
+            // for (int j = 0; j < fields.Length; j++) {
+            //     FieldInfo field = fields[j];
+            for (int j = 0; j < buttons.Length; j++) {
+                Button button = buttons[j];
+                if (button.gameObject.name != "Button" + method.Name)
+                    continue;
+                
+                button.onClick.AddListener(() => StartCoroutine((IEnumerator)method.Invoke(this, null)));
+                break;
+            }
+        }
     }
 
-    IEnumerator LoadSafeAssetBundle()
+    private IEnumerator LoadSafeAssetBundle()
     {
+        Debug.Log("LoadSafeAssetBundle");
         WWW www = new WWW(this.GetStreamingAssetsUrl("/ab_test_safecase_root"));
 
         yield return www;
@@ -38,7 +62,8 @@ public class AssetBundleLoader : MonoBehaviour
         GameObject go = www.assetBundle.LoadAsset<GameObject>("Root");
         Instantiate(go);
     }
-    IEnumerator LoadUnsafeCase1AssetBundle()
+
+    private IEnumerator LoadUnsafeCase1AssetBundle()
     {
         WWW rootWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_unsafecase1_root"));
 
@@ -58,14 +83,84 @@ public class AssetBundleLoader : MonoBehaviour
             yield break;
         }
 
-        // crash if not loaded
+        // if it is not loaded, it causes crash when try to show inspector window of Root prefab
+        // depsWww.assetBundle.LoadAsset<AnimationClip>("AnimationClip");
+
+        Debug.Log("if you try to show inspector window of Root instance, UnityEditor will crash");
+
+        GameObject go = rootWww.assetBundle.LoadAsset<GameObject>("Root");
+        Instantiate(go);
+    }
+
+    private IEnumerator LoadUnsafeCase2AssetBundle()
+    {
+        WWW rootWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_unsafecase2_root"));
+
+        yield return rootWww;
+
+        if (rootWww.error != null) {
+            Debug.Log(rootWww.error);
+            yield break;
+        }
+
+        WWW depsWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_unsafecase2_deps"));
+
+        yield return depsWww;
+
+        if (depsWww.error != null) {
+            Debug.Log(rootWww.error);
+            yield break;
+        }
+
+        // if it is not loaded, it causes crash when try to show inspector window of Dependency prefab
+        /*
+        WWW depdepWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_unsafecase2_depdep"));
+
+        yield return depdepWww;
+
+        if (depdepWww.error != null) {
+            Debug.Log(depdepWww.error);
+            yield break;
+        }
+
+        depdepWww.assetBundle.LoadAsset<AnimationClip>("AnimationClip");
+        */
+
+        depsWww.assetBundle.LoadAsset<GameObject>("Dependency");
+
+        GameObject go = rootWww.assetBundle.LoadAsset<GameObject>("Root");
+        Instantiate(go);
+    }
+
+    private IEnumerator LoadUnsafeCase3AssetBundle()
+    {
+        WWW rootWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_unsafecase3_root"));
+
+        yield return rootWww;
+
+        if (rootWww.error != null) {
+            Debug.Log(rootWww.error);
+            yield break;
+        }
+
+        WWW depsWww = new WWW(this.GetStreamingAssetsUrl("/ab_test_unsafecase3_deps"));
+
+        yield return depsWww;
+
+        if (depsWww.error != null) {
+            Debug.Log(rootWww.error);
+            yield break;
+        }
+
+        // if it is not loaded, it causes crash when try to show inspector window of Dependency prefab
         // depsWww.assetBundle.LoadAsset<AnimationClip>("AnimationClip");
 
         GameObject go = rootWww.assetBundle.LoadAsset<GameObject>("Root");
         Instantiate(go);
     }
 
-    string GetStreamingAssetsUrl(string path)
+
+    private string GetStreamingAssetsUrl(string path)
     {
         return "file://" + Application.streamingAssetsPath + path;
     }
